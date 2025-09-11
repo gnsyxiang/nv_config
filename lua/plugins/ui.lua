fa159f8faa539153953801e3292b251859283208
zhenquan.qiu <gnsyxiang@163.com>
2025-09-11 17:34:31 +0800

    feat: add gruvbox.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,5 +2,10 @@
 return {
     {
         url = "git@github.com:ellisonleao/gruvbox.nvim",
+        version = "2.0.0",
+        priority = 1000,
+        config = function()
+            require("plugins.configs.gruvbox")
+        end,
     }
 }
